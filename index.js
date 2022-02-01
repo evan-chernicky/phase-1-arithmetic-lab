38dc9c6c825e5f8cb5f6cbd279e1551bccaea00d
Evan Chernicky <echernicky@gmail.com>
2022-01-31 22:32:35 -0500

    commit tree dfa70e61964e966049181d8d77e4e1497f19a9ea

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+//1
+const num1 = 31
+const num2 = 2;
+const multiply = num1 * num2;
+
+//2
+const random = Math.floor(Math.random() * 10) + 1;
+
+//3
+const num3 = 40;
+const num4 = 9;
+const mod = num3 % num4;
+
+//4
+const max = Math.max(15,16,17,18,19,20)
